92b99a9bb9b01822cc24d9669133aa1db2257b4c
Sumaiya S <sum.shefa@gmail.com>
2022-07-26 03:30:25 -0500

    Update Assignment3.java

--- Assignment3.java
+++ Assignment3.java
@@ -148,9 +148,3 @@ class Truck extends Vehicle {
                 super.manufacturer + " is " + super.vehicleNo + " and loading capacity is " + this.loadingCapacity + ".");
     }
 }
-/*
- v) The bookstore application:
-    The bookstore sells books, music CD's, and software's. Typically a customer first logs on to the system , entering a customer id and password. The customer can then browse for titles or search by keyword. The customer puts some of the titles into a shopping cart which keeps track of the desired titles. When the customer is done shopping, he/she confirms the order, shipping and billing address. The bookstore system then issues a shipping order, bills the customer and issues an electronic receipt.
-    At the end of transaction the customer logs off.
-    OO Design: With reference to the above create, a scenario
-    system diagram, classes and attributes and relationship as well */
